d56c84c75a64a03ce586109302bd2042b3f5d135
Karen Kepner <kkepner14@gmail.com>
2019-05-06 11:08:16 -0600

    Build SkiDayCounter class and render skiData on screen

--- src/index.js
+++ src/index.js
@@ -2,11 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+let skiData = {
+  total: 19,
+  powder: 10,
+  fatbiking: 20,
+  goal: 35
+}
+
+class SkiDayCounter extends React.Component {
+  getPercent = decimal => {
+    return Math.round(decimal * 100) + '%'
+  }
+  calcGoalProgress = (total, goal) => {
+    return this.getPercent(total/goal)
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>Ski Day Counter</h1>
+        <div>
+          <p>Total Days: {this.props.total}</p>
+        </div>
+        <div>
+          <p>Powder Days: {this.props.powder}</p>
+        </div>
+        <div>
+          <p>Fatbike Days: {this.props.fatbiking}</p>
+        </div>
+        <div>
+          <p>Goal Progress: {this.calcGoalProgress(this.props.total, this.props.goal)}</p>
+        </div>
+      </section>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <SkiDayCounter 
+    total={skiData.total}
+    powder={skiData.powder}
+    fatbiking={skiData.fatbiking}
+    goal={skiData.goal}/>,
+  document.getElementById('root')
+)
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
